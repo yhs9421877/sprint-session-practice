99dcbc6092be3c17448e26f1dcc80a45caadde12
Hoyong Lee <igotoweb@gmail.com>
2021-04-14 19:19:55 +0900

    docs: fix test descriptions

--- server-session/__tests__/server.test.js
+++ server-session/__tests__/server.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-describe('파일 및 환경변수 테스트', () => {
+describe('파일 및 환경 변수 테스트', () => {
   it('인증서 파일이 존재해야 합니다', () => {
     // fs.existsSync 함수가 사용하는 상대경로는 package.json 파일의 위치를 기준으로 합니다.
     // 서버 package.json의 위치에 인증서 파일들을 위치시켜야 합니다.
@@ -7,7 +7,7 @@ describe('파일 및 환경변수 테스트', () => {
     expect(fs.existsSync('./cert.pem')).to.be.true
   })
 
-  it('환경변수에 데이터베이스 비밀번호가 존재해야 합니다', () => {
+  it('환경 변수에 데이터베이스 비밀번호가 존재해야 합니다', () => {
     expect(process.env.DATABASE_PASSWORD).to.exist;
     expect(process.env.DATABASE_PASSWORD).not.to.include(';')
   })
@@ -41,7 +41,7 @@ describe('Authentication - Server', () => {
 
 
 
-    it('성공적으로 데이터베이스와 연결되어야 합니다', async () => {
+    it('성공적으로 데이터베이스에 연결해야 합니다', async () => {
       let response;
 
       console.log('DB configurations');
@@ -101,38 +101,38 @@ describe('Authentication - Server', () => {
 
         resCookies = correctResponse.header['set-cookie'][0];
       });
-      it("로그인 요청시 전달받은 유저 아이디 혹은 비밀번호가 잘못된 경우, 'not authorized'메세지가 응답에 포함되어야 합니다", async () => {
+      it("로그인 요청시 잘못된 사용자 아이디 혹은 비밀번호를 전달할 경우, `not authorized` 메세지가 응답에 포함되어야 합니다", async () => {
         expect(failedResponse.body.message).to.eql('not authorized');
       });
 
-      it("로그인 요청시 전달받은 유저아이디, 비밀번호가 데이터베이스에 저장된 정보와 완벽히 일치하는 경우, 'ok'메세지가 응답에 포함되어야 합니다", async () => {
+      it("로그인 요청시 정확한 사용자 아이디와 비밀번호를 전달할 경우, `ok` 메세지가 응답에 포함되어야 합니다", async () => {
         expect(correctResponse.body.message).to.eql('ok');
       });
 
       describe('쿠키 옵션', () => {
 
-        it('쿠키옵션중 Domain 옵션은 `localhost`로 설정되어야 합니다', () => {
+        it('쿠키 옵션중 Domain 옵션은 `localhost`로 설정되어야 합니다', () => {
           expect(resCookies).include('Domain=localhost;');
         });
 
-        it('쿠키옵션중 Path 옵션은 `/`로 설정되어야 합니다', () => {
+        it('쿠키 옵션중 Path 옵션은 `/`로 설정되어야 합니다', () => {
           expect(resCookies).include('Path=/;');
         });
 
-        it('쿠키옵션중 HttpOnly 옵션이 설정되어야 합니다', () => {
+        it('쿠키 옵션중 HttpOnly 옵션이 설정되어야 합니다', () => {
           expect(resCookies).include('HttpOnly');
         });
 
-        it('쿠키옵션중 Secure 옵션이 설정되어야 합니다', () => {
+        it('쿠키 옵션중 Secure 옵션이 설정되어야 합니다', () => {
           expect(resCookies).include('Secure');
         });
 
-        it('쿠키옵션중 SameSite 옵션은 `none`으로 설정되어야 합니다', () => {
+        it('쿠키 옵션중 SameSite 옵션은 `none`으로 설정되어야 합니다', () => {
           expect(resCookies).include('SameSite=None');
         });
       });
 
-      it('`express-session` 모듈이 암호화 한 내용을 값으로 가지는 `connect.sid`쿠키가 존재해야 합니다', async () => {
+      it('세션 아이디를 전달하기 위한 `connect.sid` 쿠키가 존재해야 합니다 (이 쿠키는 express-session 모듈이 만듭니다)', async () => {
         expect(resCookies).include('connect.sid');
       });
     });
@@ -159,7 +159,7 @@ describe('Authentication - Server', () => {
         expect(response.status).to.eql(200);
       });
 
-      it('로그아웃 이후 혹은 로그인 전에 로그아웃을  요청하는경우, 상태코드 400을 리턴해야 합니다', async () => {
+      it('로그아웃 이후 혹은 로그인 전에 로그아웃을 요청하는 경우, 상태코드 400을 리턴해야 합니다', async () => {
         await agent.post('/users/logout').set('Cookie', resCookies);
 
         const response = await agent.post('/users/logout');
@@ -182,7 +182,7 @@ describe('Authentication - Server', () => {
         resCookies = response.header['set-cookie'][0];
       });
 
-      it('로그인 이후 유저정보를 요청하는 경우 상태코드 200 및 `ok`메세지를 리턴해야 합니다', async () => {
+      it('로그인 이후 사용자 정보를 요청하는 경우 상태코드 200 및 `ok` 메세지를 리턴해야 합니다', async () => {
         const response = await agent
           .get('/users/userinfo')
           .set('Cookie', resCookies);
@@ -191,7 +191,7 @@ describe('Authentication - Server', () => {
         expect(response.body.message).to.eql('ok');
       });
 
-      it('로그인이 되지 않은 상태에서 유저정보를 요청하는 경우, 상태코드 400 및 `not authorized`메세지를 리턴해야 합니다', async () => {
+      it('로그인이 되지 않은 상태에서 사용자 정보를 요청하는 경우, 상태코드 400 및 `not authorized` 메세지를 리턴해야 합니다', async () => {
         await agent.post('/users/logout').set('Cookie', resCookies);
 
         const response = await agent.get('/users/userinfo');
